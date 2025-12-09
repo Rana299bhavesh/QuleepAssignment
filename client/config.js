532c6f6682c87f937c833549b81a874b543adee3
Rana bhavesh <ranabhavesh299@gmail.com>
2025-12-09 11:11:30 +0530

    changes in config.js

--- client/config.js
+++ client/config.js
@@ -1,9 +1,9 @@
 // config.js
 const config = {
 
-  
-  API_URL: import.meta.env.PROD ? "" : "http://localhost:5000/api"
-
+  API_URL: import.meta.env.PROD 
+    ? "/api"  
+    : "http://localhost:5000/api"
 };
 
 export default config;
